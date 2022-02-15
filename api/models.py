8603a7981a394e82dd7d933e27a93ef6ecdd1e85
sanjitmathew <sanjitmathewranjit@gmail.com>
2022-02-15 12:37:53 +0530

    a user can have multiple teams

--- api/models.py
+++ api/models.py
@@ -6,8 +6,14 @@ class Teams(models.Model):
     name = models.CharField(max_length=128)
     is_active = models.BooleanField()
 
+    def __str__(self):
+        return self.name
+
 class Users(models.Model):
     name = models.CharField(max_length=254)
     email = models.EmailField(max_length=254)
     ph_no = models.CharField(max_length=10)
-    team_id = models.ForeignKey(Teams, on_delete=models.CASCADE)
+    team_id = models.ManyToManyField(Teams)
+
+    def __str__(self):
+        return self.name
